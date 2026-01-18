9cdc62b22ff98e833e85ad291d0efcf45e9e7628
Ziad Mustafa Eid <ziadeid.web@gmail.com>
2026-01-18 21:37:06 +0200

    creating the base of our project

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+    <App />
+)
+
+
+// npm i react-router tailwindcss @tailwindcss/vite -D daisyui@latest
+
